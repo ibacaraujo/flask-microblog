471074a97e21e74cfad9e5a0c5d31c1720c00c22
Iago Breno <ibacaraujo@gmail.com>
2019-07-24 18:23:41 +0000

    Add templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,26 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Iago Breno'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Kawhi'},
+            'body': 'Excited to play in Los Angeles!'
+        },
+        {
+            'author': {'username': 'LeBron'},
+            'body': 'Working hard based on a recovery approach!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
